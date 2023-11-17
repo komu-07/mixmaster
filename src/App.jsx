e4712774c38c133f9eafc8ab12cb5593bf21265b
komu <sanjeevniraj2001@gmail.com>
2023-11-17 20:30:38 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import {
+  About,
+  Landing,
+  NewsLetter,
+  HomeLayout,
+  Error,
+  SinglePageError,
+  Cocktail,
+} from './pages';
+
+import { loader as landingLoader } from './pages/Landing';
+import { loader as cocktailPageLoader } from './pages/Cocktail';
+import { action as newsletterAction } from './pages/Newsletter';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        loader: landingLoader(queryClient),
+        errorElement: <SinglePageError />,
+      },
+      {
+        path: 'cocktail/:id',
+        errorElement: <SinglePageError />,
+        loader: cocktailPageLoader(queryClient),
+        element: <Cocktail />,
+      },
+      {
+        path: 'newsletter',
+        action: newsletterAction,
+        element: <NewsLetter />,
+      },
+
+      {
+        path: 'about',
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />;
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+export default App;
---
+++ src/components/CocktailList.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import CocktailCard from '../components/CocktailCard';
+import styled from 'styled-components';
+
+const CocktailList = ({ drinks }) => {
+  if (!drinks) {
+    return (
+      <h4 style={{ textAlign: 'center' }}>No matching cocktails found...</h4>
+    );
+  }
+  const formattedDrinks = drinks.map((item) => {
+    const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = item;
+    return {
+      id: idDrink,
+      name: strDrink,
+      image: strDrinkThumb,
+      info: strAlcoholic,
+      glass: strGlass,
+    };
+  });
+  return (
+    <Wrapper>
+      {formattedDrinks.map((item) => {
+        return <CocktailCard key={item.id} {...item} />;
+      })}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 2rem;
+`;
+
+export default CocktailList;
